5b4ff80ba4d6f4a9505045c1cad27fbedbbf571b
Soonho Kwon <percy3368@gmail.com>
2023-06-10 02:49:29 +0900

    :sparkles: Implement `StackRouter`

---
+++ src/lib/stack-router/Stack.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { pop } from "svelte-spa-router";
+
+  export let title = "";
+</script>
+
+<main transition:fly={{x: "100%", opacity: 1}}>
+    <header>
+        <button on:click={pop}>back</button>
+        <h1>{title}</h1>
+    </header>
+    <slot></slot>
+</main>
+
+<style>
+    main {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 101;
+        background-color: #FFFFFF;
+        box-shadow: var(--shadow);
+    }
+
+    header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+</style>
