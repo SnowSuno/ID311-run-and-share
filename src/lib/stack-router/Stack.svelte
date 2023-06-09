f9ae1eeb338f8fba6eebd5bf641d22a93ad12da6
Soonho Kwon <percy3368@gmail.com>
2023-06-10 03:16:31 +0900

    :lipstick: Update stack style

--- src/lib/stack-router/Stack.svelte
+++ src/lib/stack-router/Stack.svelte
@@ -1,13 +1,16 @@
 <script>
   import { fly } from "svelte/transition";
   import { pop } from "svelte-spa-router";
+  import LeftChevron from "~/assets/icons/LeftChevron.svelte";
 
   export let title = "";
 </script>
 
 <main transition:fly={{x: "100%", opacity: 1}}>
     <header>
-        <button on:click={pop}>back</button>
+        <button on:click={pop}>
+            <LeftChevron/>
+        </button>
         <h1>{title}</h1>
     </header>
     <slot></slot>
@@ -29,5 +32,17 @@
         display: flex;
         flex-direction: row;
         align-items: center;
+        padding-inline: 20px;
+        height: 50px;
+        gap: 12px;
+    }
+
+    button {
+        display: flex;
+    }
+
+    h1 {
+        font-size: 22px;
+        font-weight: 600;
     }
 </style>
