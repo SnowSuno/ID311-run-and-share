be9c3c74f2980adf2e3c8da5e275df52780595bc
Soonho Kwon <percy3368@gmail.com>
2023-06-13 01:01:13 +0900

    :sparkles: Add orientation variants in sheet component

--- src/components/elements/Sheet.svelte
+++ src/components/elements/Sheet.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
-  export let orientation: "top" | "bottom" = "top";
+  export let header;
+  export let top;
+  export let bottom;
 </script>
 
-<div class={orientation}>
-    <slot></slot>
+<div class:header class:top class:bottom>
+  <slot></slot>
 </div>
 
 <style>
     div {
-        position: absolute;
-        top: var(--header-h);
+        position: fixed;
+        left: 0;
         width: 100%;
         background-color: #FFFFFF;
-        border-radius: 0 0 var(--radius-lg) var(--radius-lg);
-        box-shadow: var(--shadow);
-        z-index: 1;
+        z-index: 200;
+    }
+
+    .header, .top {
         padding: 6px var(--inline) 22px;
+        box-shadow: 0 5px 5px rgba(0, 0, 0, 0.05);
+        border-radius: 0 0 var(--radius-lg) var(--radius-lg);
+    }
+
+    .header {
+        top: var(--header-h);
+    }
+
+    .top {
+        top: 0;
+    }
+
+    .bottom {
+        padding: 22px var(--inline) 6px;
+        box-shadow: 0 -5px 5px rgba(0, 0, 0, 0.05);
+        bottom: var(--safe-b);
+        border-radius: var(--radius-lg) var(--radius-lg) 0 0;
     }
 </style>
