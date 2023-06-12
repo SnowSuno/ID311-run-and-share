f63ee2cc2a7cfafc48bf864188f55444c6971663
Soonho Kwon <percy3368@gmail.com>
2023-06-13 01:22:31 +0900

    :lipstick: Style login page

--- src/screens/Login.svelte
+++ src/screens/Login.svelte
@@ -3,11 +3,31 @@
   import { auth, googleProvider } from "~/firebase/config";
   import { push } from "svelte-spa-router";
   import { onMount } from "svelte";
+  import MainButton from "../components/elements/MainButton.svelte";
+  import { Logo, Sprint } from "~/assets/icons";
 
   const login = () => signInWithPopup(auth, googleProvider);
   onMount(() => push("/"));
 </script>
 
 <main>
-    <button on:click={login}>Sign in</button>
+  <Sprint size={150}/>
+  <Logo/>
+  <MainButton float on:click={login}>Sign in with Google</MainButton>
 </main>
+
+<style>
+    main {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0 var(--inline) 30%;
+        gap: 24px
+    }
+</style>
