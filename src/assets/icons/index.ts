e85ab4c38eee9493b92dc79581fc0ccba566ec54
Soonho Kwon <percy3368@gmail.com>
2023-06-13 07:52:22 +0900

    :sparkles: Implement pan button

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export { default as Sprint } from "./Sprint.svelte";
 export { default as Leaderboard } from "./Leaderboard.svelte";
 export { default as User } from "./User.svelte";
 export { default as LeftChevron } from "./LeftChevron.svelte";
+export { default as CurrentLocation } from "./CurrentLocation.svelte";
